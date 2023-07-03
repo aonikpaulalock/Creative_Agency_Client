4d1ef07907acc41211d58fd989b1431de80faa1c
aonikpaulalock <alockpaul9845@gmail.com>
2023-07-03 18:42:50 +0600

    aded contck page almost done

--- src/Pages/Contact/Contact_General_Inquiries.jsx
+++ src/Pages/Contact/Contact_General_Inquiries.jsx
@@ -2,8 +2,38 @@ import React from 'react';
 
 const Contact_General_Inquiries = () => {
   return (
-    <div>
-      <h1>Contact_General_Inquiries</h1>
+    <div className='row'>
+      <div className='col'>
+        <h6 className='contact_form_head'>If you have any questions don’t be afraid to drop us a line. Leave us a little info, and we’ll be in touch.
+        </h6>
+        <form className='form-container'>
+          <div className='d-flex justify-content-between align-items-center my-4 pt-5'>
+            <div class="form-group w-100 me-5">
+              <label for="exampleInputName" className='input_lebel'>Name*</label>
+              <input type="text" class="footer-input Blogs_input contact_input" id="exampleInputName" aria-describedby="emailHelp" placeholder="Your Name" />
+            </div>
+            <div class="form-group w-100">
+              <label for="exampleInputEmail" className='input_lebel'>Email*</label>
+              <input type="email" class="footer-input Blogs_input contact_input" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Your working email" />
+            </div>
+          </div>
+          <div className='d-flex justify-content-between align-items-center'>
+            <div class="form-group w-100 me-5">
+              <label for="exampleInputCompany" className='input_lebel'>Company</label>
+              <input type="text" class="footer-input Blogs_input contact_input" id="exampleInputCompany" aria-describedby="emailHelp" placeholder="Your company name" />
+            </div>
+            <div class="form-group w-100">
+              <label for="exampleInputProject" className='input_lebel'>Subject</label>
+              <input type="email" class="footer-input Blogs_input contact_input" id="exampleInputProject" aria-describedby="emailHelp" placeholder="Title your message" />
+            </div>
+          </div>
+          <div class="form-group w-100 mb-5 mt-4">
+            <label for="exampleInputTextarea" className='input_lebel'>Message</label>
+            <textarea name="" id="exampleInputTextarea" placeholder="Your message" className='footer-input Blogs_input contact_input'></textarea>
+          </div>
+          <button className='banner-button'>Send message</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/Pages/Contact/Contact_Join_Team.jsx
+++ src/Pages/Contact/Contact_Join_Team.jsx
@@ -2,8 +2,59 @@ import React from 'react';
 
 const Contact_Join_Team = () => {
   return (
-    <div>
-      <h1>Contact_Join_Team</h1>
+    <div className='row'>
+      <div className="col">
+        <h6 className='contact_form_head'>We believe great work happens when great people work together. That's why we're always looking for the next great person.
+          <div className="accordion-container">
+            <div className="accordion-container">
+              <div className="accordion" id="myAccordion">
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button collapsed collapse-design" data-bs-toggle="collapse" data-bs-target="#collapseOne">
+                      <span className='ms-4 '>
+                        Copywriter
+                      </span>
+                    </button>
+                  </h2>
+                  <div id="collapseOne" className="accordion-collapse collapse  show" data-bs-parent="#myAccordion">
+                    <div className="card-body">
+                      <p className="accordion-text">We're looking for a Copywriter with proven experience in the creation of both short-form and long-form content, including blogs, white papers, ad copy, product listings, website copy, script writing, email and social media.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingTwo">
+                    <button type="button" className="accordion-button collapse-design" data-bs-toggle="collapse" data-bs-target="#collapseTwo">
+                      <span className='ms-4'>
+                      Paid media strategist
+                      </span>
+                    </button>
+                  </h2>
+                  <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                    <div className="card-body">
+                      <p className="accordion-text">We're looking for a Copywriter with proven experience in the creation of both short-form and long-form content, including blogs, white papers, ad copy, product listings, website copy, script writing, email and social media.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingThree">
+                    <button type="button" className="accordion-button collapsed collapse-design" data-bs-toggle="collapse" data-bs-target="#collapseThree">
+                      <span className='ms-4'>
+                      Data analyst
+                      </span>
+                    </button>
+                  </h2>
+                  <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                    <div className="card-body">
+                      <p className="accordion-text">Adipiscing nunc arcu enim elit mattis eu placerat proin. Imperdiet elementum faucibus dignissim purus. Fusce parturient diam magna ullamcorper morbi semper massa ac facilisis.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </h6>
+      </div>
     </div>
   );
 };
